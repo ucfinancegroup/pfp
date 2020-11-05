0736bd798abeeb875f07669accdfc70feaaf014b
c650 <c650@users.noreply.github.com>
2020-11-05 21:24:24 +0000

    setup user session stuff

--- server/src/controllers/user_controller.rs
+++ server/src/controllers/user_controller.rs
@@ -1,9 +1,32 @@
 pub use crate::models::user_model;
 
-use actix_web::{Responder, HttpResponse, post};
+use serde::{Deserialize};
+use actix_web::{Responder, HttpResponse, HttpRequest, web::Json, post, web::Data};
+use actix_session::Session;
+
+use rand::Rng;
+
+#[derive(Deserialize)]
+pub struct SignupPayload {
+  email: String,
+  password: String,
+  first_name: String,
+  last_name: String,
+}
 
 #[post("/signup")]
-pub async fn signup() -> impl Responder {
+pub async fn signup(session: Session, signup_payload: Json<SignupPayload>) -> impl Responder {
+  // check if user's email exists
+
+  // validate email
+
+  // validate password
+
+  // validate name
+
+  let key = rand::thread_rng().gen::<[u8; 32]>();
+  session.set("sid", std::str::from_utf8(&key).unwrap().to_string());
+
   HttpResponse::Ok().body("hi")
 }
 
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,22 +1,30 @@
 mod controllers;
 mod models;
 
-#[allow(unused_imports)]
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, App, HttpResponse, HttpServer, Responder, middleware};
+use actix_session::CookieSession;
 
 #[get("/")]
 async fn root_route() -> impl Responder {
-    HttpResponse::Ok().body("hello world")
+  HttpResponse::Ok().body("hello world")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(root_route)
-            .configure(controllers::configure)
-    })
-    .bind("0.0.0.0:8080")?
-    .run()
-    .await
+  HttpServer::new(|| {
+    App::new()
+      .wrap(
+        CookieSession::signed(&[0; 32])
+        .domain("https://finchapp.eastus.cloudapp.azure.com/")
+        .name("sid")
+        .path("/")
+        .secure(true)
+      )
+      .wrap(middleware::Logger::default())
+      .configure(controllers::configure)
+      .service(root_route)
+  })
+  .bind("0.0.0.0:8080")?
+  .run()
+  .await
 }

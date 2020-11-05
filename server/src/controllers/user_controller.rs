3abe6ba3a04825446b8e45950233c55259025fc7
c650 <c650@users.noreply.github.com>
2020-11-05 21:38:17 +0000

    logout skeleton

--- server/src/controllers/user_controller.rs
+++ server/src/controllers/user_controller.rs
@@ -1,7 +1,7 @@
 pub use crate::models::user_model;
 
 use serde::{Deserialize};
-use actix_web::{Responder, HttpResponse, HttpRequest, web::Json, post, web::Data};
+use actix_web::{Responder, HttpResponse, HttpRequest, web::Json, get, post, web::Data};
 use actix_session::Session;
 
 use rand::Rng;
@@ -14,23 +14,42 @@ pub struct SignupPayload {
   last_name: String,
 }
 
+trait Validation {
+  fn validate(&self) -> Result<(), String>;
+}
+
+impl Validation for SignupPayload {
+  fn validate(&self) -> Result<(), String> {
+    return Ok(())
+  }
+}
+
 #[post("/signup")]
 pub async fn signup(session: Session, signup_payload: Json<SignupPayload>) -> impl Responder {
+  if let Err(error_msg) = signup_payload.validate() {
+    return HttpResponse::BadRequest().json(error_msg);
+  }
+
   // check if user's email exists
 
-  // validate email
 
-  // validate password
-
-  // validate name
 
   let key = rand::thread_rng().gen::<[u8; 32]>();
   session.set("sid", std::str::from_utf8(&key).unwrap().to_string());
 
-  HttpResponse::Ok().body("hi")
+  HttpResponse::Ok().json("Success")
 }
+
+#[get("/logout")]
+pub async fn logout(session: Session) -> impl Responder {
+  session.set("sid", "");
+  HttpResponse::Ok().json("Success")
+}
+
+
 
 use actix_web::web::ServiceConfig;
 pub fn init_routes(config: &mut ServiceConfig) {
   config.service(signup);
+  config.service(logout);
 }

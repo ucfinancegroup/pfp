884d4ec54fe40e4385d67bddd72ad8ab54fdcd59
Will Chen <wchen6255@knights.ucf.edu>
2020-11-08 13:19:58 -0500

    Connection to mongo

--- server/src/main.rs
+++ server/src/main.rs
@@ -4,6 +4,8 @@ mod models;
 
 use actix_session::CookieSession;
 use actix_web::{get, middleware, App, HttpResponse, HttpServer, Responder};
+use mongodb::{bson::doc, Client};
+use dotenv::dotenv;
 
 #[get("/")]
 async fn root_route() -> impl Responder {
@@ -12,6 +14,22 @@ async fn root_route() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+  dotenv().ok();
+  let uri = dotenv::var("DATABASE_URL").expect("DATABASE_URL is not set in .env file");
+  let dbUser = dotenv::var("DATABASE_USER").expect("DATABASE_USER is not set in .env file");
+  let dbPW = dotenv::var("DATABASE_PW").expect("DATABASE_PW is not set in .env file");
+  let dbName = dotenv::var("DATABASE_NAME").expect("DATABASE_NAME is not set in .env file");
+
+  let client = Client::with_uri_str(&format!("mongodb+srv://{}:{}@{}/{}?w=majority", dbUser,  dbPW,  uri, dbName)).await.expect("Failed to initialize client.");
+  let db = client.database("Finch");
+
+  db.run_command(doc! {"ping": 1}, None).await.expect("Failed to ping client");
+  println!("Connected successfully.");
+
+  for coll_name in db.list_collection_names(None).await.expect("Failed to print collections."){
+    println!("collection: {}", coll_name);
+  }
+
   HttpServer::new(|| {
     App::new()
       .wrap(

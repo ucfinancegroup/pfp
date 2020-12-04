6ae2ab8a8d5483b78ed10354f88dbc7aabfd1ee2
c650 <c650@users.noreply.github.com>
2020-12-04 14:06:52 -0500

    secrets wip

--- server/src/services/mod.rs
+++ server/src/services/mod.rs
@@ -1,2 +1,3 @@
 pub mod db;
 pub mod finchplaid;
+pub mod secrets;
---
+++ server/src/services/secrets/mod.rs
@@ -0,0 +1,23 @@
+use serde::Deserialize;
+use std::error::Error;
+use std::fs::File;
+use std::io::BufReader;
+
+#[derive(Deserialize)]
+pub struct Environment {
+  pub database_url: String,
+  pub database_user: String,
+  pub database_pw: String,
+  pub database_name: String,
+  pub plaid_client_id: String,
+  pub plaid_sandbox_secret: String,
+}
+
+impl Environment {
+  pub fn new() -> Result<Environment, Box<dyn Error>> {
+    let file = File::open("/run/secrets/config.json")?;
+    let reader = BufReader::new(file);
+    let env = serde_json::from_reader(reader)?;
+    Ok(env)
+  }
+}

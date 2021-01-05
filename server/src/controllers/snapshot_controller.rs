ce3d06afa97ef01b494d4684beebec5f1a24bc73
c650 <c650@users.noreply.github.com>
2021-01-05 14:26:32 -0500

    refactor snapshot_controller for user FromRequest

--- server/src/controllers/snapshot_controller.rs
+++ server/src/controllers/snapshot_controller.rs
@@ -1,23 +1,15 @@
 use crate::models::user_model::User;
-use crate::services::{finchplaid::ApiClient, sessions::SessionService, users::UserService};
-use actix_session::Session;
+use crate::services::{finchplaid::ApiClient, users::UserService};
 use actix_web::{get, web::Data, HttpResponse};
 use std::sync::{Arc, Mutex};
 
 #[get("/snapshots")]
 pub async fn get_snapshots(
-  session: Session,
+  mut user: User,
   user_service: Data<UserService>,
-  session_service: Data<SessionService>,
   plaid_client: Data<Arc<Mutex<ApiClient>>>,
 ) -> HttpResponse {
-  crate::common::into_response_res(match session_service.get_valid_session(&session).await {
-    Err(e) => Err(e),
-    Ok(finch_session) => {
-      let mut user: User = user_service.new_from_session(finch_session).await.unwrap();
-      user_service.get_snapshots(&mut user, plaid_client).await
-    }
-  })
+  crate::common::into_response_res(user_service.get_snapshots(&mut user, plaid_client).await)
 }
 
 // you add the services here.

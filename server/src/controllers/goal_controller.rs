9b95190827cdabaa4bf7c2f86195723e976f6f09
c650 <c650@users.noreply.github.com>
2021-01-11 17:41:29 -0500

    goal service and controller

--- server/src/controllers/goal_controller.rs
+++ server/src/controllers/goal_controller.rs
@@ -1,4 +1,124 @@
-use crate::models::goal_model::*;
+use crate::models::{goal_model::*, user_model::User};
+use crate::services::{goals::GoalService, users::UserService};
+use actix_web::{
+  delete, get, post, put,
+  web::{Data, Path},
+  HttpResponse,
+};
+use actix_web_validator::Json;
+use serde::{Deserialize, Serialize};
+use validator::{Validate, ValidationError};
+
+#[derive(Validate, Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[validate(schema(function = "validate_goal_new_payload", skip_on_field_errors = false))]
+pub struct GoalNewPayload {
+  #[serde(rename = "name")]
+  pub name: String,
+  #[serde(rename = "start")]
+  pub start: i64,
+  #[serde(rename = "end")]
+  pub end: i64,
+  #[serde(rename = "threshold")]
+  pub threshold: i64,
+  #[serde(rename = "metric")]
+  pub metric: GoalMetrics,
+}
+
+fn validate_goal_new_payload(data: &GoalNewPayload) -> Result<(), ValidationError> {
+  if data.start >= data.end {
+    return Err(ValidationError::new(
+      "Start time of Goal should be strictly before End time.",
+    ));
+  }
+
+  Ok(())
+}
+
+impl Into<Goal> for GoalNewPayload {
+  fn into(self) -> Goal {
+    Goal {
+      id: None,
+      name: self.name,
+      start: self.start,
+      end: self.end,
+      threshold: self.threshold,
+      metric: self.metric,
+    }
+  }
+}
+
+#[post("/goal/new")]
+pub async fn new_goal(
+  user: User,
+  user_service: Data<UserService>,
+  payload: Json<GoalNewPayload>,
+) -> HttpResponse {
+  crate::common::into_response_res(
+    GoalService::new_goal(payload.into_inner(), user, user_service).await,
+  )
+}
+
+#[get("/goals")]
+pub async fn get_goals(user: User) -> HttpResponse {
+  crate::common::into_response(user.goals)
+}
+
+#[get("/goal/{id}")]
+pub async fn get_goal(user: User, id: Path<String>) -> HttpResponse {
+  crate::common::into_response_res(GoalService::get_goal(id.into_inner(), user).await)
+}
+
+#[put("/goal/{id}")]
+pub async fn update_goal(
+  user: User,
+  user_service: Data<UserService>,
+  id: Path<String>,
+  payload: Json<GoalNewPayload>,
+) -> HttpResponse {
+  crate::common::into_response_res(
+    GoalService::update_goal(id.into_inner(), payload.into_inner(), user, user_service).await,
+  )
+}
+
+#[delete("/goal/{id}")]
+pub async fn delete_goal(
+  user: User,
+  user_service: Data<UserService>,
+  id: Path<String>,
+) -> HttpResponse {
+  crate::common::into_response_res(
+    GoalService::delete_goal(id.into_inner(), user, user_service).await,
+  )
+}
+
+// "examples" can never be an ID, and we will put the service ahead of the others
+// currently, we take a User to make this an authorised route... Should we?
+#[get("/goal/examples")]
+pub async fn get_goal_examples(_: User) -> HttpResponse {
+  crate::common::into_response(vec![
+    GoalNewPayload {
+      name: "Save 100 Dollars".to_string(),
+      start: 1609977600,
+      end: 1617753600,
+      threshold: 10000, // dollars times 100
+      metric: GoalMetrics::Savings,
+    },
+    GoalNewPayload {
+      name: "Spend under 100 Dollars".to_string(),
+      start: 1609977600,
+      end: 1617753600,
+      threshold: -10000, // dollars times 100
+      metric: GoalMetrics::Spending,
+    },
+  ])
+}
 
 use actix_web::web::ServiceConfig;
-pub fn init_routes(config: &mut ServiceConfig) {}
+pub fn init_routes(config: &mut ServiceConfig) {
+  config.service(get_goal_examples);
+  config.service(get_goal);
+  config.service(get_goals);
+  config.service(new_goal);
+  config.service(delete_goal);
+  config.service(update_goal);
+}
---
+++ server/src/services/goals/mod.rs
@@ -0,0 +1,202 @@
+#[allow(non_snake_case)]
+pub mod GoalService {
+  use crate::common::errors::ApiError;
+  use crate::controllers::goal_controller::GoalNewPayload;
+  use crate::models::{
+    goal_model::*,
+    user_model::{Snapshot, User},
+  };
+  use crate::services::users::UserService;
+  use actix_web::web::Data;
+  use wither::{mongodb::bson::oid::ObjectId, Model};
+
+  pub fn calculate_goal_progress(
+    goal: Goal,
+    snapshots: Vec<Snapshot>,
+  ) -> Result<GoalAndStatus, ApiError> {
+    let last_before_start = snapshots.iter().fold(Snapshot::default(), |acc, cur| {
+      if cur.snapshot_time < goal.start {
+        cur.clone()
+      } else {
+        acc
+      }
+    });
+
+    let last_in_range = snapshots.iter().fold(Snapshot::default(), |acc, cur| {
+      if cur.snapshot_time > goal.end {
+        acc
+      } else {
+        cur.clone()
+      }
+    });
+
+    let change = match goal.metric {
+      GoalMetrics::Income => {
+        last_in_range.running_income.amount - last_before_start.running_income.amount
+      }
+      GoalMetrics::Savings => {
+        last_in_range.running_savings.amount - last_before_start.running_savings.amount
+      }
+      GoalMetrics::Spending => {
+        last_in_range.running_spending.amount - last_before_start.running_spending.amount
+      }
+    };
+
+    if change == goal.threshold {
+      return Ok(GoalAndStatus {
+        progress: 100.0,
+        goal,
+      });
+    }
+
+    Ok(GoalAndStatus {
+      progress: (change as f64) / (goal.threshold.clone() as f64),
+      goal,
+    })
+  }
+
+  pub async fn get_goal(goal_id: String, user: User) -> Result<GoalAndStatus, ApiError> {
+    let goal = retrieve_goal(goal_id, user.goals)?;
+    calculate_goal_progress(goal, user.snapshots)
+  }
+
+  pub fn retrieve_goal(goal_id: String, goals: Vec<Goal>) -> Result<Goal, ApiError> {
+    let goal_id_opt = Some(
+      ObjectId::with_string(goal_id.as_str())
+        .or(Err(ApiError::new(400, "Malformed Object Id".to_string())))?,
+    );
+
+    let found = goals.into_iter().find(|rec| rec.id == goal_id_opt).clone();
+
+    found.ok_or(ApiError::new(
+      400,
+      format!("No goal with id {} found in current user", goal_id),
+    ))
+  }
+
+  pub async fn new_goal(
+    payload: GoalNewPayload,
+    mut user: User,
+    user_service: Data<UserService>,
+  ) -> Result<GoalAndStatus, ApiError> {
+    let mut goal: Goal = payload.into();
+    goal.set_id(ObjectId::new());
+
+    user.goals.push(goal.clone());
+
+    user_service.save(&mut user).await?;
+
+    calculate_goal_progress(goal, user.snapshots)
+  }
+
+  pub async fn update_goal(
+    goal_id: String,
+    payload: GoalNewPayload,
+    mut user: User,
+    user_service: Data<UserService>,
+  ) -> Result<GoalAndStatus, ApiError> {
+    let goal_id_opt = Some(
+      ObjectId::with_string(goal_id.as_str())
+        .or(Err(ApiError::new(400, "Malformed Object Id".to_string())))?,
+    );
+
+    let mut goal: Goal = payload.into();
+    goal.set_id(goal_id_opt.unwrap().clone());
+
+    let updated = user
+      .goals
+      .iter_mut()
+      .find(|rec| rec.id == goal.id)
+      .ok_or(ApiError::new(
+        400,
+        format!("No goal with id {} found in current user", goal_id),
+      ))
+      .and_then(|rec| {
+        *rec = goal.clone();
+        Ok(goal)
+      })?;
+
+    user_service.save(&mut user).await?;
+
+    calculate_goal_progress(updated, user.snapshots)
+  }
+
+  pub async fn delete_goal(
+    goal_id: String,
+    mut user: User,
+    user_service: Data<UserService>,
+  ) -> Result<GoalAndStatus, ApiError> {
+    let goal_id_opt = Some(
+      ObjectId::with_string(goal_id.as_str())
+        .or(Err(ApiError::new(400, "Malformed Object Id".to_string())))?,
+    );
+
+    let removed = user
+      .goals
+      .iter()
+      .position(|rec| rec.id == goal_id_opt)
+      .ok_or(ApiError::new(
+        400,
+        format!("No goal with id {} found in current user", goal_id),
+      ))
+      .and_then(|pos| Ok(user.goals.swap_remove(pos)))?;
+
+    user_service.save(&mut user).await?;
+
+    calculate_goal_progress(removed, user.snapshots)
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  use crate::common::Money;
+  use crate::models::{goal_model::*, user_model::Snapshot};
+
+  #[test]
+  fn test_calculate_progress() {
+    let saving_goal = Goal {
+      id: None,
+      name: "Save 100 Dollars".to_string(),
+      start: 3,
+      end: 10,
+      threshold: 10000, // dollars times 100
+      metric: GoalMetrics::Savings,
+    };
+
+    let snapshots = vec![
+      Snapshot {
+        net_worth: Money { amount: 0 },
+        running_spending: Money { amount: 0 },
+        running_savings: Money { amount: 0 },
+        running_income: Money { amount: 0 },
+        snapshot_time: 1,
+      },
+      Snapshot {
+        net_worth: Money { amount: 0 },
+        running_spending: Money { amount: 0 },
+        running_savings: Money { amount: 5000 },
+        running_income: Money { amount: 0 },
+        snapshot_time: 6,
+      },
+    ];
+
+    let progress = GoalService::calculate_goal_progress(saving_goal, snapshots.clone()).unwrap();
+
+    assert_eq!(0.5 as f64, progress.progress);
+
+    let spending_goal = Goal {
+      id: None,
+      name: "Spend under 100 Dollars".to_string(),
+      start: 5,
+      end: 9,
+      threshold: -10000, // dollars times 100
+      metric: GoalMetrics::Spending,
+    };
+
+    let progress2 = GoalService::calculate_goal_progress(spending_goal, snapshots.clone()).unwrap();
+
+    assert_eq!(0.0 as f64, progress2.progress);
+  }
+}

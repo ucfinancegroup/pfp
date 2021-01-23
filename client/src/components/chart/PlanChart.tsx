638e2ba92c3a6946a4a54f1dbb509313233a4d5a
Heath <heath@heathmilligan.net>
2021-01-23 16:13:27 -0500

    Get d3 chart example working

--- client/src/components/chart/PlanChart.tsx
+++ client/src/components/chart/PlanChart.tsx
@@ -15,135 +15,141 @@ export function PlanChart(props: PlanChartProps) {
     const height = 440;
     const width = 1000;
     const margin = ({top: 20, right: 20, bottom: 30, left: 40});
-    const [data, setData] = useState<any>();
+   // const [data, setData] = useState<any>();
 
     useEffect(() => {
         getData();
+
     }, []);
-
-    if (!data) {
-        return <p>Loading...</p>
-    }
-
     async function getData() {
         const d = await d3.csv("/data.csv");
-        console.log(d);
-        const tmpData = Object.assign((d).map(({date, close}) => ({date, value: close})), {y: "↑ Close $"});
-        setData(tmpData);
-    }
+        const data = Object.assign((d).map(({date, close}) =>
+            ({date: new Date(date), value: parseFloat(close)})), {y: "↑ Close $"});
+        //setData(tmpData);
 
-    function createChart() {
-        const svg = d3.create("svg")
-            .attr("viewBox", [0, 0, width, height] as any)
-            .style("display", "block");
 
-        const clipId = "clip" + Math.random().toString();
+        const area = (x, y) => d3.area()
+            .defined((d: any) => !isNaN(d.value))
+            .x((d: any)  => x(d.date))
+            .y0(y(0))
+            .y1((d: any)  => y(d.value));
 
-        svg.append("clipPath")
-            .attr("id", clipId)
-            .append("rect")
-            .attr("x", margin.left)
-            .attr("y", 0)
-            .attr("height", height)
-            .attr("width", width - margin.left - margin.right);
+        const x = d3.scaleUtc()
+            .domain(d3.extent(data,  (d: any) => d.date) as any)
+            .range([margin.left, width - margin.right]);
 
-        const gx = svg.append("g");
+        const y = d3.scaleLinear()
+            .domain([0, d3.max(data,  (d: any) => d.value)] as any)
+            .range([height - margin.bottom, margin.top]);
 
-        const gy = svg.append("g");
+        const xAxis = (g, x, height) => g
+            .attr("transform", `translate(0,${height - margin.bottom})`)
+            .call(d3.axisBottom(x).ticks(width / 80).tickSizeOuter(0));
 
-        const path = svg.append("path")
-            .datum(data)
-            .attr("clip-path", clipId)
-            .attr("fill", "steelblue");
+        const yAxis = (g, y, title) => g
+            .attr("transform", `translate(${margin.left},0)`)
+            .call(d3.axisLeft(y))
+            .call(g => g.select(".domain").remove())
+            .call(g => g.selectAll(".title").data([title]).join("text")
+                .attr("class", "title")
+                .attr("x", -margin.left)
+                .attr("y", 10)
+                .attr("fill", "currentColor")
+                .attr("text-anchor", "start")
+                .text(title));
 
-        return Object.assign(svg.node(), {
-            update(focusX, focusY) {
+        function createChart() {
+            const svg = d3.create("svg")
+                .attr("viewBox", [0, 0, width, height] as any)
+                .style("display", "block");
+
+            const clipId = "clip" + Math.round(Math.random() * 10000);
+
+            svg.append("clipPath")
+                .attr("id", clipId)
+                .append("rect")
+                .attr("x", margin.left)
+                .attr("y", 0)
+                .attr("height", height)
+                .attr("width", width - margin.left - margin.right);
+
+            const gx = svg.append("g");
+
+            const gy = svg.append("g");
+
+            const path = svg.append("path")
+                .datum(data)
+                .attr("clip-path", "url(#" + clipId + ")")
+                .attr("fill", "steelblue");
+
+            const node = svg.node();
+            (node as any).update = function(focusX, focusY) {
                 gx.call(xAxis, focusX, height);
                 gy.call(yAxis, focusY, data.y);
                 path.attr("d", area(focusX, focusY) as any);
+            };
+            return node;
+        }
+
+        const chart = createChart();
+
+        function createFocus() {
+            const svg = d3.create("svg")
+                .attr("viewBox", [0, 0, width, focusHeight] as any)
+                .style("display", "block");
+
+            const brush = d3.brushX()
+                .extent([[margin.left, 0.5], [width - margin.right, focusHeight - margin.bottom + 0.5]])
+                .on("brush", brushed)
+                .on("end", brushended);
+
+            const defaultSelection = [x(d3.utcYear.offset(x.domain()[1], -1)), x.range()[1]];
+
+            svg.append("g")
+                .call(xAxis, x, focusHeight);
+
+            svg.append("path")
+                .datum(data)
+                .attr("fill", "steelblue")
+                .attr("d", area(x, y.copy().range([focusHeight - margin.bottom, 4])) as any);
+
+            const gb = svg.append("g")
+                .call(brush)
+                .call(brush.move, defaultSelection);
+
+            function brushed({selection}) {
+                if (selection) {
+                    svg.property("value", selection.map(x.invert, x).map(d3.utcDay.round));
+                    svg.dispatch("input");
+                    requestAnimationFrame(() => {
+                        update();
+                    });
+                }
             }
-        });
-    }
 
-    const chart = createChart();
-
-
-
-    function update() {
-        const [minX, maxX] = focus as any;
-        const maxY = d3.max(data, (d: any) => minX <= d.date && d.date <= maxX ? d.value as any : NaN);
-        chart.update(x.copy().domain(focus as any), y.copy().domain([0, maxY] as any));
-    }
-
-
-    function createFocus() {
-        const svg = d3.create("svg")
-            .attr("viewBox", [0, 0, width, height] as any)
-            .style("display", "block");
-
-        const clipId = "clip" + Math.random().toString();
-
-        svg.append("clipPath")
-            .attr("id", clipId)
-            .append("rect")
-            .attr("x", margin.left)
-            .attr("y", 0)
-            .attr("height", height)
-            .attr("width", width - margin.left - margin.right);
-
-        const gx = svg.append("g");
-
-        const gy = svg.append("g");
-
-        const path = svg.append("path")
-            .datum(data)
-            .attr("clip-path", clipId)
-            .attr("fill", "steelblue");
-
-        return Object.assign(svg.node(), {
-            update(focusX, focusY) {
-                gx.call(xAxis, focusX, height);
-                gy.call(yAxis, focusY, data.y);
-                path.attr("d", area(focusX, focusY));
+            function brushended({selection}) {
+                if (!selection) {
+                    gb.call(brush.move, defaultSelection);
+                }
             }
-        });
+
+            return svg.node();
+        }
+
+        const focus = createFocus();
+
+        function update() {
+            const [minX, maxX] = (focus as any).value as any;
+            const maxY = d3.max(data, (d: any) => minX <= d.date && d.date <= maxX ? d.value as any : NaN);
+            (chart as any).update(x.copy().domain((focus as any).value as any), y.copy().domain([0, maxY] as any));
+
+            document.getElementById("d3test").innerHTML = "";
+            document.getElementById("d3test").appendChild(chart);
+            document.getElementById("d3test").appendChild(focus);
+        }
+
+        update();
     }
-
-    const focus = createFocus();
-    const area = (x, y) => d3.area()
-        .defined((d: any) => !isNaN(d.value))
-        .x((d: any)  => x(d.date))
-        .y0(y(0))
-        .y1((d: any)  => y(d.value) );
-
-    const x = d3.scaleUtc()
-        .domain(d3.extent(data,  (d: any) => d.date) as any)
-        .range([margin.left, width - margin.right]);
-
-    const y = d3.scaleLinear()
-        .domain([0, d3.max(data,  (d: any) => d.value)] as any)
-        .range([height - margin.bottom, margin.top]);
-
-    const xAxis = (g, x, height) => g
-        .attr("transform", `translate(0,${height - margin.bottom})`)
-        .call(d3.axisBottom(x).ticks(width / 80).tickSizeOuter(0));
-
-    const yAxis = (g, y, title) => g
-        .attr("transform", `translate(${margin.left},0)`)
-        .call(d3.axisLeft(y))
-        .call(g => g.select(".domain").remove())
-        .call(g => g.selectAll(".title").data([title]).join("text")
-            .attr("class", "title")
-            .attr("x", -margin.left)
-            .attr("y", 10)
-            .attr("fill", "currentColor")
-            .attr("text-anchor", "start")
-            .text(title));
-
-
-    setTimeout(() => {
-       document.getElementById("d3test").appendChild(chart);
-    });
 
     return <div id="d3test">
 

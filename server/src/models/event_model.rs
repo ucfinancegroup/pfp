189adb244c3aa8e7b5f009fc06e5842ebc0b3ba2
William Chen <wchen6255@Knights.ucf.edu>
2021-01-26 14:07:58 -0500

    struct macros

--- server/src/models/event_model.rs
+++ server/src/models/event_model.rs
@@ -4,17 +4,20 @@ use serde::{Deserialize, Serialize};
 use serde_json::Map;
 use wither::{mongodb::bson::oid::ObjectId, Model};
 
+#[derive(Model, Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct Event {
     pub name: String,
     pub start: DateTime,
     pub transforms: Vec<Transfrom>,
 }
 
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct Transfrom {
     pub trigger: TimeInterval,
     pub change: Map<String, f64>,
 }
 
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct Asset {
     pub name: String,
     pub class: String,

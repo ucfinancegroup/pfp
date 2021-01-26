19c5ba477e074b9482e500879cba186c0675a72d
William Chen <wchen6255@Knights.ucf.edu>
2021-01-26 13:57:11 -0500

    fill model structs

--- server/src/models/event_model.rs
+++ server/src/models/event_model.rs
@@ -1,8 +1,22 @@
+use chrono::DateTime;
+use models::recurring_model::TimeInterval;
 use serde::{Deserialize, Serialize};
+use serde_json::Map;
 use wither::{mongodb::bson::oid::ObjectId, Model};
 
-pub struct Event {}
+pub struct Event {
+    pub name: String,
+    pub start: DateTime,
+    pub transforms: Vec<Transfrom>,
+}
 
-pub struct Transfrom {}
+pub struct Transfrom {
+    pub trigger: TimeInterval,
+    pub change: Map<String, f64>,
+}
 
-pub struct Asset {}
+pub struct Asset {
+    pub name: String,
+    pub class: String,
+    pub annualized_performance: String,
+}

d4e5c9501716445615a52d588ce0bc0f59f4186a
William Chen <wchen6255@Knights.ucf.edu>
2021-01-26 14:31:37 -0500

    add plans fields

--- server/src/models/plan_model.rs
+++ server/src/models/plan_model.rs
@@ -1,14 +1,24 @@
 use chrono::DateTime;
-use models::recurring_model::TimeInterval;
+use models::recurring_model::{Recurring, TimeInterval};
 use serde::{Deserialize, Serialize};
 use serde_json::Map;
 use wither::{mongodb::bson::oid::ObjectId, Model};
 
 #[derive(Model, Clone, Debug, PartialEq, Serialize, Deserialize)]
-pub struct Plan {}
+pub struct Plan {
+    pub name: String,
+    pub recurrings: Vec<Recurring>,
+    pub allocations: Vec<Allocation>,
+    pub events: Vec<Event>,
+}
 
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
-pub struct Allocation {}
+pub struct Allocation {
+    pub description: String,
+    pub date: DateTime,
+    pub assets: Vec<Asset>,
+    pub allocation_changes: Vec<f64>,
+}
 
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct Event {
@@ -20,7 +30,8 @@ pub struct Event {
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct Transfrom {
     pub trigger: TimeInterval,
-    pub change: Map<String, f64>,
+    pub assets: Vec<Asset>,
+    pub changes: Vec<f64>,
 }
 
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]

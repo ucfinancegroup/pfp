88b4205ef3924a32e53e567612ec99a7a2eae9bf
c650 <c650@users.noreply.github.com>
2021-01-26 15:37:59 -0500

    refactor similar user insights to track savings,spending,income

--- server/src/services/insights/similar_user.rs
+++ server/src/services/insights/similar_user.rs
@@ -11,6 +11,80 @@ use wither::{
   Model,
 };
 
+#[derive(Clone, Copy)]
+struct SimilarUserMetrics {
+  pub spending_less: i64,
+  pub savings_less: i64,
+  pub income_less: i64,
+  pub total_similar_users: i64,
+}
+
+impl Default for SimilarUserMetrics {
+  fn default() -> Self {
+    SimilarUserMetrics {
+      spending_less: 0,
+      savings_less: 0,
+      income_less: 0,
+      total_similar_users: 0,
+    }
+  }
+}
+
+impl std::ops::Add for SimilarUserMetrics {
+  type Output = SimilarUserMetrics;
+
+  fn add(self, other: Self) -> Self::Output {
+    SimilarUserMetrics {
+      spending_less: self.spending_less + other.spending_less,
+      savings_less: self.savings_less + other.savings_less,
+      income_less: self.income_less + other.income_less,
+      total_similar_users: self.total_similar_users + other.total_similar_users,
+    }
+  }
+}
+
+#[derive(Clone)]
+struct UserMetricRates {
+  pub spending_rate: f64,
+  pub savings_rate: f64,
+  pub income_rate: f64,
+}
+
+impl UserMetricRates {
+  pub fn new(start: &Snapshot, end: &Snapshot) -> Self {
+    if start.snapshot_time == end.snapshot_time {
+      return Self::default();
+    }
+
+    let elapsed = (end.snapshot_time - start.snapshot_time) as f64;
+
+    UserMetricRates {
+      spending_rate: (end.running_spending - start.running_spending) / elapsed,
+      savings_rate: (end.running_savings - start.running_savings) / elapsed,
+      income_rate: (end.running_income - start.running_income) / elapsed,
+    }
+  }
+
+  pub fn compare(&self, other: &Self) -> SimilarUserMetrics {
+    SimilarUserMetrics {
+      spending_less: (self.spending_rate > other.spending_rate) as i64,
+      savings_less: (self.savings_rate > other.savings_rate) as i64,
+      income_less: (self.income_rate > other.income_rate) as i64,
+      total_similar_users: 1,
+    }
+  }
+}
+
+impl Default for UserMetricRates {
+  fn default() -> Self {
+    UserMetricRates {
+      spending_rate: 0.0,
+      savings_rate: 0.0,
+      income_rate: 0.0,
+    }
+  }
+}
+
 pub fn match_income_range(u: &User) -> bson::Document {
   doc! {
     "$match": {
@@ -62,10 +136,10 @@ pub async fn generate_similar_user_insight(
 
   let extracted_snapshots = agg.map(extract_snapshots);
 
-  let (less, total): (i32, i32) =
+  let metrics: SimilarUserMetrics =
     compare_snapshots_to_user(&user.snapshots, extracted_snapshots, &since).await;
 
-  if total <= 0 {
+  if metrics.total_similar_users <= 0 {
     return Err(AppError::new("No peers for insight generation"));
   }
 
@@ -74,7 +148,7 @@ pub async fn generate_similar_user_insight(
     format!(
       "Your savings over the last {} days puts you above {}% of similar users!",
       lookback.num_days(),
-      100 * less / total
+      100 * metrics.savings_less / metrics.total_similar_users
     ),
     InsightTypes::Savings,
     None,
@@ -102,28 +176,25 @@ async fn compare_snapshots_to_user<
   users_snapshots: &Vec<Snapshot>,
   similar_users_snapshots: S,
   since: &DateTime<Utc>,
-) -> (i32, i32) {
+) -> SimilarUserMetrics {
   let my_rate = calculate_rates(users_snapshots, since);
 
-  let res: (i32, i32) = similar_users_snapshots
-    .fold((0, 0), |(less, total), snaps| async move {
-      snaps.map_or_else(
-        |_| (less, total),
+  let res: SimilarUserMetrics = similar_users_snapshots
+    .fold(SimilarUserMetrics::default(), |metrics, snaps| {
+      futures::future::ready(snaps.map_or_else(
+        |_| metrics,
         |snaps| {
           let rate = calculate_rates(&snaps, since);
-
-          let new_less = less + if rate < my_rate { 1 } else { 0 };
-
-          (new_less, total + 1)
+          metrics + my_rate.compare(&rate)
         },
-      )
+      ))
     })
     .await;
 
   res
 }
 
-fn calculate_rates(snapshots: &Vec<Snapshot>, since: &DateTime<Utc>) -> f64 {
+fn calculate_rates(snapshots: &Vec<Snapshot>, since: &DateTime<Utc>) -> UserMetricRates {
   let tstamp = since.timestamp();
   let filtered = snapshots
     .iter()
@@ -133,19 +204,10 @@ fn calculate_rates(snapshots: &Vec<Snapshot>, since: &DateTime<Utc>) -> f64 {
   let f = filtered.first();
   let l = filtered.last();
 
-  let rate = match (f, l) {
-    (Some(first), Some(last)) => {
-      if first.snapshot_time != last.snapshot_time {
-        let diff: f64 = (last.running_savings.amount - first.running_savings.amount) as f64;
-        diff / (last.snapshot_time - first.snapshot_time) as f64
-      } else {
-        0.0
-      }
-    }
-    _ => 0.0,
-  };
-
-  rate
+  match (f, l) {
+    (Some(first), Some(last)) => UserMetricRates::new(&first, &last),
+    _ => UserMetricRates::default(),
+  }
 }
 
 #[cfg(test)]
@@ -209,14 +271,14 @@ mod tests {
       },
     ];
 
-    let (less, total): (i32, i32) = compare_snapshots_to_user(
+    let metrics: SimilarUserMetrics = compare_snapshots_to_user(
       &this_users_snapshots,
       futures::stream::iter(other_users_snapshots.into_iter()),
       &DateTime::<Utc>::from_utc(NaiveDateTime::from_timestamp(1, 0), Utc),
     )
     .await;
 
-    assert_eq!(less, 1);
-    assert_eq!(total, 2);
+    assert_eq!(metrics.savings_less, 1);
+    assert_eq!(metrics.total_similar_users, 2);
   }
 }

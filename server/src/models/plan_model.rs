34e80bb9a4e8a7e377b0c4360e059cf97175a2ad
William Chen <39677488+wanchichen@users.noreply.github.com>
2021-01-26 20:37:06 -0500

    Plans models (#137)
    * Create event_model.rs
    
    * stub
    
    * fill model structs
    
    * struct macros
    
    * rename file, add plans
    
    * add plans fields
    
    * fix field type
    
    * update transforms model in yaml
    
    * update allocations model in yaml
    
    * add object to wrap asset change
    
    * update models for asset change
    
    * update yaml with new obj
    
    * change trigger type to time interval
    
    * separate allocation change struct
    
    * validate allocation value range
    
    * change validation value to float
    
    * update yaml with new struct

---
+++ server/src/models/plan_model.rs
@@ -0,0 +1,55 @@
+use chrono::DateTime;
+use models::recurring_model::{Recurring, TimeInterval};
+use serde::{Deserialize, Serialize};
+use serde_json::Map;
+use wither::{mongodb::bson::oid::ObjectId, Model};
+
+#[derive(Model, Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub struct Plan {
+    #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
+    pub id: Option<ObjectId>,
+    pub name: String,
+    pub recurrings: Vec<Recurring>,
+    pub allocations: Vec<Allocation>,
+    pub events: Vec<Event>,
+}
+
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub struct Allocation {
+    pub description: String,
+    pub date: DateTime,
+    pub schema: Vec<AllocationChange>,
+}
+
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub struct Event {
+    pub name: String,
+    pub start: DateTime,
+    pub transforms: Vec<Transfrom>,
+}
+
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub struct Transfrom {
+    pub trigger: TimeInterval,
+    pub changes: Vec<AssetChange>,
+}
+
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub struct Asset {
+    pub name: String,
+    pub class: String,
+    pub annualized_performance: f64,
+}
+
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub struct AssetChange {
+    pub asset: Asset,
+    pub change: f64,
+}
+
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub struct AllocationChange {
+    pub asset: Asset,
+    #[validate(range(min = 0.0, max = 100.0))]
+    pub change: f64,
+}

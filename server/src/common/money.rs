fb387158542ac58d6afd07ef3ec522c6f189509f
c650 <c650@users.noreply.github.com>
2021-01-31 13:35:32 -0500

    money lib

--- server/src/common/money.rs
+++ server/src/common/money.rs
@@ -1,28 +1,60 @@
+use rust_decimal::Decimal;
 use serde::{Deserialize, Serialize};
+use std::ops::{Add, Div, Mul, Sub};
 
-#[derive(Serialize, Deserialize, Debug, Clone, Copy, PartialEq)]
+#[derive(Serialize, Deserialize, Debug, Clone, Copy, Eq, PartialEq)]
 pub struct Money {
-  pub amount: i64,
+  pub amount: Decimal,
 }
 
-impl From<f64> for Money {
-  fn from(f: f64) -> Money {
-    Money {
-      amount: (f * 100.0).floor() as i64,
-    }
+impl Money {
+  pub fn new<T: Into<Decimal>>(d: T) -> Self {
+    Self { amount: d.into() }
   }
 }
 
-impl Into<f64> for Money {
-  fn into(self) -> f64 {
-    (self.amount as f64) / 100.0
+impl Default for Money {
+  fn default() -> Self {
+    Money::new(Decimal::new(0, 0))
   }
 }
 
-impl std::ops::Sub for Money {
-  type Output = f64;
+// impl Into<Decimal> for Money {
+//   fn into(self) -> Decimal {
+//     self.amount
+//   }
+// }
 
-  fn sub(self, other: Self) -> Self::Output {
-    ((self.amount - other.amount) as f64) / 100.0
+impl<T: Into<Decimal>> From<T> for Money {
+  fn from(d: T) -> Self {
+    Self::new(d)
+  }
+}
+
+impl<T: Into<Money>> Add<T> for Money {
+  type Output = Money;
+  fn add(self, other: T) -> Self::Output {
+    Money::new(self.amount + other.into().amount)
+  }
+}
+
+impl<T: Into<Money>> Sub<T> for Money {
+  type Output = Money;
+  fn sub(self, other: T) -> Self::Output {
+    Money::new(self.amount - other.into().amount)
+  }
+}
+
+impl<T: Into<Money>> Mul<T> for Money {
+  type Output = Money;
+  fn mul(self, other: T) -> Self::Output {
+    Money::new(self.amount * other.into().amount)
+  }
+}
+
+impl<T: Into<Money>> Div<T> for Money {
+  type Output = Money;
+  fn div(self, other: T) -> Self::Output {
+    Money::new(self.amount / other.into().amount)
   }
 }

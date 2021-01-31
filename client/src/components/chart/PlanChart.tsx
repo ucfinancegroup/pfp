1a58ca7d5fdba80c8525e6eaf4fdcd4bbe91b64d
Heath <heath@heathmilligan.net>
2021-01-31 13:09:22 -0500

    Display text and rectangles for recurrings on graph

--- client/src/components/chart/PlanChart.tsx
+++ client/src/components/chart/PlanChart.tsx
@@ -16,6 +16,66 @@ export function PlanChart(props: PlanChartProps) {
     const height = 440;
     const width = 1000;
     const margin = ({top: 20, right: 20, bottom: 30, left: 40});
+    const predictionStart = new Date("2011-01-01");
+
+
+    const recurrings: GraphRecurring[] = [
+        {
+            name: "College Job",
+            start: new Date("2010-05-01"),
+            end: new Date("2011-05-14"),
+            color: '#ffa7cf',
+        },
+        {
+            name: "Test",
+            start: new Date("2011-04-01"),
+            end: new Date("2011-07-14"),
+            color: '#dda7ff',
+        },
+        {
+            name: "Test Overlap 1",
+            start: new Date("2011-07-22"),
+            end: new Date("2011-09-30"),
+            color: '#c9ffa7',
+        },
+        {
+            name: "Test Overlap 2",
+            start: new Date("2011-10-15"),
+            end: new Date("2011-12-03"),
+            color: '#ffa7f6',
+        },
+        {
+            name: "Test 2",
+            start: new Date("2010-06-04"),
+            end: new Date("2012-01-29"),
+            color: '#ffdca7',
+        },
+        {
+            name: "Another recurring",
+            start: new Date("2011-05-04"),
+            end: new Date("2011-08-25"),
+            color: '#a7ffd2',
+        },
+        {
+            name: "Long Recurring",
+            start: new Date("2008-07-24"),
+            end: new Date("2010-03-14"),
+            color: '#a7d9ff',
+        },
+
+        {
+            name: "Recurring 3",
+            start: new Date("2009-04-13"),
+            end: new Date("2010-03-03"),
+            color: '#a7fff5',
+        },
+        {
+            name: "Final Recurring",
+            start: new Date("2012-02-24"),
+            end: new Date("2012-03-14"),
+            color: '#ffa7a7',
+        }
+    ];
 
     useEffect(() => {
         getData();
@@ -24,16 +84,20 @@ export function PlanChart(props: PlanChartProps) {
     async function getData() {
         const d = await d3.csv("/data.csv");
         const data = Object.assign((d).map(({date, close}) =>
-            ({date: new Date(date), value: parseFloat(close)})), {y: "↑ Close $"});
+            ({date: new Date(date), value: parseFloat(close)})));
 
-        /*
+        const knownData = data.filter(f => f.date <= predictionStart);
+        const predictedData = data.filter(f => f.date >= predictionStart);
+
+        console.log(knownData, predictedData);
+
+
+
         const area = (x, y) => d3.area()
             .defined((d: any) => !isNaN(d.value))
             .x((d: any)  => x(d.date))
             .y0(y(0))
             .y1((d: any)  => y(d.value));
-
-         */
 
         const line = (x, y) => d3.area()
             .curve(curveBasis)
@@ -45,8 +109,9 @@ export function PlanChart(props: PlanChartProps) {
             .domain(d3.extent(data,  (d: any) => d.date) as any)
             .range([margin.left, width - margin.right]);
 
+        const maxY = d3.max(data,  (d: any) => d.value);
         const y = d3.scaleLinear()
-            .domain([0, d3.max(data,  (d: any) => d.value)] as any)
+            .domain([0, maxY] as any)
             .range([height - margin.bottom, margin.top]);
 
         const xAxis = (g, x, height) => g
@@ -71,10 +136,34 @@ export function PlanChart(props: PlanChartProps) {
                 .attr("viewBox", [0, 0, width, height] as any)
                 .style("display", "block");
 
-            const clipId = "clip" + Math.round(Math.random() * 10000);
+            svg.append("linearGradient")
+                .attr("id", "areaGradient")
+                .attr("gradientUnits", "userSpaceOnUse")
+                .attr("x1", 0).attr("y1", 0)
+                .attr("x2", 0).attr("y2", maxY)
+                .selectAll("stop")
+                .data([
+                    {offset: "0%", color: "#21c19c", opacity: 0},
+                    {offset: "100%", color: "#21c19c", opacity: 1},
+                ])
+                .enter().append("stop")
+                .attr("stop-opacity", function(d) { return d.opacity; })
+                .attr("offset", function(d) { return d.offset; })
+                .attr("stop-color", function(d) { return d.color; });
+
+            const clipId = "clipPath";
 
             svg.append("clipPath")
                 .attr("id", clipId)
+                .append("rect")
+                .attr("x", margin.left)
+                .attr("y", 0)
+                .attr("height", height)
+                .attr("width", width - margin.left - margin.right);
+
+
+            svg.append("clipPath")
+                .attr("id", "rectClip")
                 .append("rect")
                 .attr("x", margin.left)
                 .attr("y", 0)
@@ -85,16 +174,33 @@ export function PlanChart(props: PlanChartProps) {
 
             const gy = svg.append("g");
 
-            const path = svg.append("path")
-                .datum(data)
+            const knownPath = svg.append("path")
+                .datum(knownData)
                 .attr("clip-path", "url(#" + clipId + ")")
-                .attr("class", styles.path);
+                .attr("class", styles.path + " " + styles["path--known"]);
+
+            const knownArea = svg.append("path")
+                .datum(knownData)
+                .attr("clip-path", "url(#" + clipId + ")")
+                .attr("class", styles.area);
+
+            const predictedPath = svg.append("path")
+                .datum(predictedData)
+                .attr("clip-path", "url(#" + clipId + ")")
+                .attr("class", styles.path + " " + styles["path--predicted"]);
+
 
             const node = svg.node();
+
+
             (node as any).update = function(focusX, focusY) {
                 gx.call(xAxis, focusX, height);
                 gy.call(yAxis, focusY, data.y);
-                path.attr("d", line(focusX, focusY) as any);
+                knownArea.attr("d", area(focusX, focusY) as any);
+                knownPath.attr("d", line(focusX, focusY) as any);
+                predictedPath.attr("d", line(focusX, focusY) as any);
+
+                createRects(svg, focusX, false, height - margin.bottom);
             };
             return node;
         }
@@ -118,13 +224,20 @@ export function PlanChart(props: PlanChartProps) {
                 .call(xAxis, x, focusHeight);
 
             svg.append("path")
-                .datum(data)
+                .datum(knownData)
                 .attr("d", line(x, y.copy().range([focusHeight - margin.bottom, 4])) as any)
-                .attr("class", styles.path);
+                .attr("class", styles.path+ " " + styles["path--known"]);
+
+            svg.append("path")
+                .datum(predictedData)
+                .attr("d", line(x, y.copy().range([focusHeight - margin.bottom, 4])) as any)
+                .attr("class", styles.path + " " + styles["path--predicted"]);
 
             const gb = svg.append("g")
                 .call(brush)
                 .call(brush.move, defaultSelection);
+
+            createRects(svg, x, true, focusHeight - 30);
 
             function brushed({selection}) {
                 if (selection) {
@@ -157,10 +270,88 @@ export function PlanChart(props: PlanChartProps) {
             document.getElementById("d3test").appendChild(focus);
         }
 
+
+        function createRects(svg: d3.Selection<SVGSVGElement, undefined, null, undefined>, x: Function, mini: boolean, height: number) {
+            svg.selectAll('.rects').remove();
+
+            const betweenPadding = 2;
+            const bottom = height - betweenPadding; // Padding
+
+            const sortedRecurrings = recurrings.sort((a, b) => a.start.getTime() - b.start.getTime());
+
+            for (let a of sortedRecurrings) {
+                a.level = -1;
+            }
+
+            // Slow overlap algo
+            for (let a of sortedRecurrings) {
+                let level = 0;
+
+                for (let b of sortedRecurrings) {
+                    const overlap = a.end > b.start && a.start < b.end;
+
+                    if (overlap && level === b.level)
+                        level++;
+                }
+
+                a.level = level;
+            }
+
+            const rects = svg.append('g')
+                .attr('class', 'rects');
+
+            if (!mini) {
+                rects.attr("clip-path", "url(#rectClip)");
+            }
+
+            for (let recurring of sortedRecurrings) {
+                const rectLeft = x(recurring.start);
+                const rectRight = x(recurring.end);
+                const rectHeight = !mini ? 20 : 5;
+                const cornerRadius = !mini ? 5 : 2;
+
+                const rectWidth = rectRight - rectLeft;
+                let y = bottom - rectHeight;
+                y -= recurring.level * (rectHeight + betweenPadding);
+
+                const g = rects.append('g')
+                    .attr('class', 'rect')
+                    .attr('transform', `translate(${rectLeft},${y})`)
+
+
+
+
+                g.append('rect')
+                    .attr('rx', cornerRadius)
+                    .attr('rx', cornerRadius)
+                    .attr('width', rectWidth)
+                    .attr('height', rectHeight)
+                    .attr('fill', recurring.color);
+
+                if (!mini) {
+                    g.append('text')
+                        .attr('x', 6)
+                        .attr('y', 15)
+                        .attr('fill', 'black')
+                        .attr('font-size', "14px")
+                        .text(recurring.name);
+                }
+            }
+        }
+
         update();
     }
 
     return <div id="d3test">
 
     </div>
+}
+
+type GraphRecurring = {
+    start: Date;
+    end: Date;
+    color: string;
+    name: string;
+
+    level?: number;
 }

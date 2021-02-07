0e7054be0e49a94befe0cd276a7a0f650d7cd00c
c650 <c650@users.noreply.github.com>
2021-02-07 13:05:10 -0500

    bday validator >=13yo

--- server/src/common/mod.rs
+++ server/src/common/mod.rs
@@ -1,10 +1,12 @@
 pub mod errors;
-
 mod money;
-pub use money::*;
-
 use actix_web::HttpResponse;
+use chrono::{Datelike, TimeZone, Utc};
+pub use money::*;
+use rust_decimal::Decimal;
+use rust_decimal_macros::dec;
 use serde::Serialize;
+use validator::ValidationError;
 
 pub fn into_response<T>(m: T) -> HttpResponse
 where
@@ -34,9 +36,6 @@ where
     .clone()
 }
 
-use rust_decimal::Decimal;
-use rust_decimal_macros::dec;
-use validator::ValidationError;
 pub fn decimal_at_least_zero(d: &Decimal) -> Result<(), ValidationError> {
   match *d >= dec!(0) {
     true => Ok(()),
@@ -48,5 +47,32 @@ pub fn decimal_between_zero_or_hundred(d: &Decimal) -> Result<(), ValidationErro
   match *d >= dec!(0) && *d <= dec!(100) {
     true => Ok(()),
     false => Err(ValidationError::new("Field must be between 0 and 100")),
+  }
+}
+
+pub fn min_age_13yo(birthday: &String) -> Result<(), ValidationError> {
+  log::debug!("Got birthday {} ", birthday);
+
+  let date = chrono::NaiveDate::parse_from_str(birthday, "%Y-%m-%d")
+    .map_err(|e| {
+      log::warn!("{}", e);
+      ValidationError::new("invalid birthday format. Expected YYYY-mm-dd")
+    })
+    .and_then(|date| Ok(Utc.from_local_datetime(&date.and_hms(0, 0, 0))))?
+    .earliest()
+    .ok_or(ValidationError::new(
+      "Could not extract valid UTC dateTime from birthday",
+    ))?;
+
+  let mut thirteen_years_ago = Utc::now();
+  thirteen_years_ago = thirteen_years_ago
+    .with_year(thirteen_years_ago.year() - 13)
+    .unwrap();
+
+  log::debug!("13y ago {}, parsed {}", thirteen_years_ago, date);
+
+  match date < thirteen_years_ago {
+    true => Ok(()),
+    false => Err(ValidationError::new("Must be at least 13yo to sign up")),
   }
 }

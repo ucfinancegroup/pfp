87823188ea9715686901683af5ab824942448dcc
c650 <c650@users.noreply.github.com>
2021-02-15 12:35:36 -0500

    fin products resovle account flow

--- server/src/services/financial_products/mod.rs
+++ server/src/services/financial_products/mod.rs
@@ -1,5 +1,5 @@
 use crate::common::errors::ApiError;
-use crate::models::financial_product_model::*;
+use crate::models::{financial_product_model::*, user_model::AccountRecord};
 use crate::services::db::DatabaseService;
 use plaid::models::Account;
 use wither::{
@@ -19,15 +19,33 @@ impl FinProductService {
     FinProductService { db: db.db.clone() }
   }
 
-  pub async fn resolve_account(&self, account: &Account) -> Result<FinancialProduct, ApiError> {
-    let name = account
+  pub fn make_account_record(
+    item_id: String,
+    account: &Account,
+    product: &Option<FinancialProduct>,
+  ) -> Result<AccountRecord, ApiError> {
+    Ok(AccountRecord {
+      item_id,
+      account_id: account.account_id.clone(),
+      account_name: Self::get_account_name(account)?,
+      hidden: false,
+      known_account_id: product.as_ref().map(|p| p.id()).flatten(),
+    })
+  }
+
+  fn get_account_name(account: &Account) -> Result<String, ApiError> {
+    account
       .official_name
       .clone()
       .or(Some(account.name.clone()))
       .ok_or(ApiError::new(
         500,
         "Could not get name for Account".to_string(),
-      ))?;
+      ))
+  }
+
+  pub async fn resolve_account(&self, account: &Account) -> Result<FinancialProduct, ApiError> {
+    let name = Self::get_account_name(account)?;
 
     self.resolve_with_name(&name).await
   }

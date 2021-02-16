e3d76faf886fb6377136ebb1989a5e589c864b58
William Chen <wchen6255@Knights.ucf.edu>
2021-02-16 13:57:00 -0500

    payload

--- server/src/services/plans/mod.rs
+++ server/src/services/plans/mod.rs
@@ -1,2 +1,16 @@
 #[allow(non_snake_case)]
-pub mod PlansService {}
+pub mod PlansService {
+    use crate::common::errors::ApiError;
+    use crate::controllers::plans_controller::{PlanNewPayload, PlanUpdatePayload};
+    use crate::models::plan_model::*;
+    use crate::models::user_model::User;
+    use crate::services::users::UserService;
+    use actix_web::web::Data;
+
+    pub async fn new_plan(
+        payload: PlanNewPayload,
+        mut User: User,
+        user_service: Data<UserService>,
+    ) -> Result<Plan, ApiError> {
+    }
+}

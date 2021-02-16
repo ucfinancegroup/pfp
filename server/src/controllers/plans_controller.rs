edc2d2ce67d4ce3b62e46d1d3cfd921e4cbff721
William Chen <wchen6255@Knights.ucf.edu>
2021-02-16 13:57:16 -0500

    actual payload

--- server/src/controllers/plans_controller.rs
+++ server/src/controllers/plans_controller.rs
@@ -1,9 +1,33 @@
+use crate::models::plan_model::{Allocation, Event, Plan};
+use crate::models::recurring_model::Recurring;
 use actix_web::web::ServiceConfig;
 use actix_web::web::{Data, Path};
+use actix_web_validator::{Json, Validate};
+use serde::{Deserialize, Serialize};
 
-pub struct PlanNewPayload {}
+#[derive(Validate, Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub struct PlanNewPayload {
+    pub name: String,
+    pub recurrings: Vec<Recurring>,
+    pub allocations: Vec<Allocation>,
+    pub events: Vec<Event>,
+}
+
+impl Into<Plan> for PlanNewPayload {
+    fn into(self) -> Plan {
+        Plan {
+            id: None,
+            name: self.name,
+            recurrings: self.recurrings,
+            allocations: self.allocations,
+            events: self.events,
+        }
+    }
+}
+
 pub struct PlanUpdatePayload {}
 
+/*
 #[get("/plans/example")]
 pub async fn get_example(_: User) -> HttpResponse {}
 
@@ -15,7 +39,7 @@ pub async fn create_new_plan(user: User, payload: PlanNewPayload) -> HttpRespons
 
 #[put("/plans/edit")]
 pub async fn edit_plan(user: User, payload: PlanUpdatePayload) -> HttpResponse {}
-
+*/
 pub fn init_routes(config: &mut ServiceConfig) {
-    config.service(get_example);
+    //config.service(get_example);
 }

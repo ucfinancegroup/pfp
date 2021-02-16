5a05002d8713674cb25accc29245c80e471c1385
William Chen <wchen6255@Knights.ucf.edu>
2021-02-16 11:35:52 -0500

    stub routes

--- server/src/controllers/plans_controller.rs
+++ server/src/controllers/plans_controller.rs
@@ -1,6 +1,21 @@
+use actix_web::web::ServiceConfig;
 use actix_web::web::{Data, Path};
 
-use actix_web::web::ServiceConfig;
+pub struct PlanNewPayload {}
+pub struct PlanUpdatePayload {}
+
+#[get("/plans/example")]
+pub async fn get_example(_: User) -> HttpResponse {}
+
+#[get("/plans")]
+pub async fn get_plans(user: User) -> HttpResponse {}
+
+#[post("/plans/new")]
+pub async fn create_new_plan(user: User, payload: PlanNewPayload) -> HttpResponse {}
+
+#[put("/plans/edit")]
+pub async fn edit_plan(user: User, payload: PlanUpdatePayload) -> HttpResponse {}
+
 pub fn init_routes(config: &mut ServiceConfig) {
-    //config.service(get_example);
+    config.service(get_example);
 }

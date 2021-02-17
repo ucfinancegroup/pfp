a848a65d594acfd93d334ffbc64a057d43d08561
William Chen <wchen6255@Knights.ucf.edu>
2021-02-17 14:44:33 -0500

    plans services

--- server/src/services/plans/mod.rs
+++ server/src/services/plans/mod.rs
@@ -22,4 +22,73 @@ pub mod PlansService {
 
         Ok(plan)
     }
+
+    pub async fn get_plan(plan_id: String, user: User) -> Result<Plan, ApiError> {
+        let plan_id_opt = Some(
+            ObjectId::with_string(plan_id.as_str())
+                .or(Err(ApiError::new(400, "Malformed Object Id".to_string())))?,
+        );
+
+        let found = user
+            .plans
+            .into_iter()
+            .find(|rec| rec.id == plan_id_opt)
+            .clone();
+
+        found.ok_or(ApiError::new(
+            400,
+            format!("No plan with id {} found in current user", plan_id),
+        ))
+    }
+
+    pub async fn update_plan(
+        plan_id: String,
+        payload: PlanNewPayload,
+        mut user: User,
+        user_service: Data<UserService>,
+    ) -> Result<Plan, ApiError> {
+        let plan_id_opt = Some(
+            ObjectId::with_string(plan_id.as_str())
+                .or(Err(ApiError::new(400, "Malformed Object Id".to_string())))?,
+        );
+        let mut plan: Plan = payload.into();
+        plan.set_id(plan_id_opt.unwrap().clone());
+        let updated = user
+            .plans
+            .iter_mut()
+            .find(|rec| rec.id == plan.id)
+            .ok_or(ApiError::new(
+                400,
+                format!("No plan with id {} found in current user", plan_id),
+            ))
+            .and_then(|rec| {
+                *rec = plan.clone();
+                Ok(plan)
+            })?;
+        user_service.save(&mut user).await?;
+        Ok(updated)
+    }
+
+    pub async fn delete_plan(
+        plan_id: String,
+        mut user: User,
+        user_service: Data<UserService>,
+    ) -> Result<Plan, ApiError> {
+        let plan_id_opt = Some(
+            ObjectId::with_string(plan_id.as_str())
+                .or(Err(ApiError::new(400, "Malformed Object Id".to_string())))?,
+        );
+        let removed = user
+            .plans
+            .iter()
+            .position(|rec| rec.id == plan_id_opt)
+            .ok_or(ApiError::new(
+                400,
+                format!("No plan with id {} found in current user", plan_id),
+            ))
+            .and_then(|pos| Ok(user.plans.swap_remove(pos)))?;
+        user_service.save(&mut user).await?;
+
+        Ok(removed)
+    }
 }

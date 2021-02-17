509772bb642bf71e5c73293e78673020ca724453
c650 <c650@users.noreply.github.com>
2021-02-17 10:30:32 -0500

    change log level to debug when features=development

--- server/src/bin/api.rs
+++ server/src/bin/api.rs
@@ -31,12 +31,7 @@ cfg_if::cfg_if! {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-  simple_logger::SimpleLogger::new()
-    .with_level(log::LevelFilter::Off)
-    .with_module_level("pfp_server", log::LevelFilter::Info)
-    .with_module_level(module_path!(), log::LevelFilter::Info)
-    .init()
-    .unwrap();
+  common::finchlog::init_log(module_path!());
 
   let env: services::secrets::Environment =
     services::secrets::Environment::new().expect("Need good env config");
---
+++ server/src/common/finchlog.rs
@@ -0,0 +1,21 @@
+cfg_if::cfg_if! {
+  if #[cfg(feature="development")] {
+    fn get_log_level() -> log::LevelFilter {
+      log::LevelFilter::Debug
+    }
+  } else {
+    fn get_log_level() -> log::LevelFilter {
+      log::LevelFilter::Info
+    }
+  }
+}
+
+pub fn init_log(module: &str) -> () {
+  let log_level = get_log_level();
+  simple_logger::SimpleLogger::new()
+    .with_level(log::LevelFilter::Off)
+    .with_module_level("pfp_server", log_level)
+    .with_module_level(module, log_level)
+    .init()
+    .unwrap()
+}

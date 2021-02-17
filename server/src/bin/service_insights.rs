5412fae7ceddc3ffa2e39aa576b70db6419a5743
c650 <c650@users.noreply.github.com>
2021-02-17 14:15:53 -0500

    change log level to debug when features=development (#180)

--- server/src/bin/service_insights.rs
+++ server/src/bin/service_insights.rs
@@ -5,12 +5,7 @@ use services::{db::DatabaseService, finchplaid, insights::InsightsService, secre
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-  simple_logger::SimpleLogger::new()
-    .with_level(log::LevelFilter::Off)
-    .with_module_level("pfp_server", log::LevelFilter::Info)
-    .with_module_level(module_path!(), log::LevelFilter::Info)
-    .init()
-    .unwrap();
+  common::finchlog::init_log(module_path!());
 
   let env = Environment::new().expect("Need good env config");
 
--- server/src/common/mod.rs
+++ server/src/common/mod.rs
@@ -1,4 +1,5 @@
 pub mod errors;
+pub mod finchlog;
 mod money;
 use actix_web::HttpResponse;
 use chrono::{Datelike, TimeZone, Utc};

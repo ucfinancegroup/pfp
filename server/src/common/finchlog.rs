0d08bd24c1207ca210d2dc6b5060ca6acc22bbf4
c650 <c650@users.noreply.github.com>
2021-03-04 16:13:41 -0500

    add actix_web request logging

--- server/src/common/finchlog.rs
+++ server/src/common/finchlog.rs
@@ -14,6 +14,7 @@ pub fn init_log(module: &str) -> () {
   let log_level = get_log_level();
   simple_logger::SimpleLogger::new()
     .with_level(log::LevelFilter::Off)
+    .with_module_level("actix_web", log::LevelFilter::Info)
     .with_module_level("pfp_server", log_level)
     .with_module_level(module, log_level)
     .init()

7ff2edbf09b119b31259651b5fa569514b157712
c650 <c650@users.noreply.github.com>
2021-03-04 17:59:33 -0500

    refactor plaid client

--- server/src/services/finchplaid/mod.rs
+++ server/src/services/finchplaid/mod.rs
@@ -4,17 +4,102 @@ use crate::controllers::plaid_controller::ItemIdResponse;
 use crate::models::user_model::{PlaidItem, User};
 use crate::services::{financial_products::FinProductService, users::UserService};
 use actix_web::web::Data;
-use plaid::models::{Account, RetrieveAnItemsAccountsRequest, RetrieveAnItemsAccountsResponse};
+use plaid::apis::configuration::Configuration;
+use plaid::models::*;
 use rust_decimal::Decimal;
 use std::collections::{HashMap, HashSet};
 use std::convert::TryFrom;
-use std::sync::{Arc, Mutex};
 
+#[derive(Clone)]
 pub struct ApiClient {
   pub client_id: String,
   pub secret: String,
   pub client_name: String,
-  pub configuration: plaid::apis::configuration::Configuration,
+}
+
+impl ApiClient {
+  fn get_configuration() -> Configuration {
+    Configuration::default()
+  }
+
+  pub async fn create_link_token(
+    &self,
+    user_id: String,
+  ) -> Result<CreateLinkTokenResponse, ApiError> {
+    let config = Self::get_configuration();
+    plaid::apis::link_tokens_api::create_link_token(
+      &config,
+      plaid::models::CreateLinkTokenRequest::new(
+        self.client_id.clone(),
+        self.secret.clone(),
+        self.client_name.clone(),
+        vec!["US".to_string()],
+        "en".to_string(),
+        plaid::models::User::new(user_id),
+        vec!["auth".to_string(), "transactions".to_string()],
+      ),
+    )
+    .await
+    .or(Err(ApiError::new(500, "".to_string())))
+  }
+
+  pub async fn retrieve_an_items_accounts(
+    &self,
+    item_access_token: String,
+  ) -> Result<RetrieveAnItemsAccountsResponse, ApiError> {
+    let config = Self::get_configuration();
+    plaid::apis::item_management_api::retrieve_an_items_accounts(
+      &config,
+      RetrieveAnItemsAccountsRequest::new(
+        self.client_id.clone(),
+        self.secret.clone(),
+        item_access_token,
+      ),
+    )
+    .await
+    .or(Err(ApiError::new(
+      500,
+      "Error while getting accounts".to_string(),
+    )))
+  }
+
+  pub async fn exchange_token(
+    &self,
+    public_token: String,
+  ) -> Result<ExchangeTokenResponse, ApiError> {
+    let config = Self::get_configuration();
+
+    plaid::apis::item_creation_api::exchange_token(
+      &config,
+      plaid::models::ExchangeTokenRequest::new(
+        self.client_id.clone(),
+        self.secret.clone(),
+        public_token,
+      ),
+    )
+    .await
+    .map_err(|_| ApiError::new(500, "Plaid Exchange Token Error".to_string()))
+  }
+
+  pub async fn retrieve_transactions(
+    &self,
+    item_access_token: String,
+    date: String,
+  ) -> Result<RetrieveTransactionsResponse, ApiError> {
+    let config = Self::get_configuration();
+    plaid::apis::transactions_api::retrieve_transactions(
+      &config,
+      RetrieveTransactionsRequest::new(
+        self.client_id.clone(),
+        self.secret.clone(),
+        item_access_token,
+        date.clone(),
+        date,
+      ),
+    )
+    .await
+    .map_err(|_| ApiError::new(500, "Error while getting transactions".to_string()))
+  }
 }
 
 // (23 Feb 2021) -- only use positive-valued accounts
@@ -56,9 +141,10 @@ pub fn get_account_transaction_coefficients(accounts: &Vec<Account>) -> HashMap<
 
 pub async fn get_account_data<'a>(
   item: &PlaidItem,
-  plaid_client: Data<Arc<Mutex<ApiClient>>>,
+  plaid_client: Data<ApiClient>,
 ) -> Result<Vec<AccountSuccess>, ApiError> {
-  let accounts = get_item_accounts(item, plaid_client.clone())
+  let accounts = plaid_client
+    .retrieve_an_items_accounts(item.access_token.clone())
     .await?
     .accounts;
   let account_id_to_coeff = get_account_balance_coefficients(&accounts);
@@ -88,10 +174,13 @@ pub async fn get_account_data<'a>(
 
 pub async fn get_net_worth(
   item: &PlaidItem,
-  plaid_client: Data<Arc<Mutex<ApiClient>>>,
+  plaid_client: Data<ApiClient>,
   excluded_accounts: &HashSet<String>,
 ) -> Result<Money, ApiError> {
-  let accounts = get_item_accounts(item, plaid_client).await?.accounts;
+  let accounts = plaid_client
+    .retrieve_an_items_accounts(item.access_token.clone())
+    .await?
+    .accounts;
 
   Ok(calculate_net_worth(&accounts, excluded_accounts))
 }
@@ -114,48 +203,14 @@ pub fn calculate_net_worth(accounts: &Vec<Account>, excluded_accounts: &HashSet<
     })
 }
 
-pub async fn get_item_accounts(
-  item: &PlaidItem,
-  plaid_client: Data<Arc<Mutex<ApiClient>>>,
-) -> Result<RetrieveAnItemsAccountsResponse, ApiError> {
-  let pc = plaid_client.lock().unwrap();
-  let config = &(pc.configuration);
-
-  plaid::apis::item_management_api::retrieve_an_items_accounts(
-    &config,
-    RetrieveAnItemsAccountsRequest::new(
-      pc.client_id.clone(),
-      pc.secret.clone(),
-      item.access_token.clone(),
-    ),
-  )
-  .await
-  .map_err(|_| ApiError::new(500, "Error while getting accounts".to_string()))
-}
-
 pub async fn exchange_public_token_for_access_token(
   public_token: String,
-  plaid_client: Data<Arc<Mutex<ApiClient>>>,
+  plaid_client: Data<ApiClient>,
   user: User,
   user_service: Data<UserService>,
   fin_product_service: Data<FinProductService>,
 ) -> Result<ItemIdResponse, ApiError> {
-  let exchanged = {
-    let pc = plaid_client.lock().unwrap();
-    let config = &(pc.configuration);
-    plaid::apis::item_creation_api::exchange_token(
-      config,
-      plaid::models::ExchangeTokenRequest::new(
-        pc.client_id.clone(),
-        pc.secret.clone(),
-        public_token,
-      ),
-    )
-    .await
-    .map_err(|_| ApiError::new(500, "Plaid Client Error".to_string()))
-  }?;
-
-  use plaid::models::ExchangeTokenResponse;
+  let exchanged = plaid_client.exchange_token(public_token).await?;
 
   let ExchangeTokenResponse {
     access_token: item_access_token,

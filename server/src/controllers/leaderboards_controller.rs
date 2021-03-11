720c9a5b4132d0015047c50a06957c9eda723ebf
Andy Phan <andytphan@outlook.com>
2021-03-11 15:28:56 -0500

    format

--- server/src/controllers/leaderboards_controller.rs
+++ server/src/controllers/leaderboards_controller.rs
@@ -1,14 +1,10 @@
 use crate::models::{recurring_model::*, user_model::User};
 use crate::services::leaderboards::LeaderboardService;
-use actix_web::{
-  get,
-  web::{Path},
-  HttpResponse,
-};
+use actix_web::{get, web::Path, HttpResponse};
 
 #[get("/leaderboard/{board}")]
 pub async fn get_leaderboard(user: User, board: Path<String>) -> HttpResponse {
-    crate::common::into_response(LeaderboardService::get_ranking(board.to_string(), &user).await)
+  crate::common::into_response(LeaderboardService::get_ranking(board.to_string(), user).await)
 }
 
 use actix_web::web::ServiceConfig;

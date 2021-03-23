7372ca521d614e1c6483c7ce0209cbedfed98d9c
Andy Phan <andytphan@outlook.com>
2021-03-22 23:06:38 -0400

    Move page

---
+++ client/src/components/leaderboards/LeaderboardList.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import { Ranking, LeaderboardApi, BoardType } from "../../api";
+import handleFetchError from "../../hooks/handleFetchError";
+
+const leaderboardApi = new LeaderboardApi();
+
+export function LeaderboardList() {
+    const [rankings, setRankings] = useState<Ranking[]>();
+    const [error, setError] = useState<string>();
+
+    useEffect(() => {
+        getRankings();
+    }, []);
+
+    async function getRankings() {
+        try {
+            let results = [];
+            for (let type in BoardType) {
+                let ranking = await leaderboardApi.getLeaderboard({ type: BoardType.Savings });
+                results.push(ranking);
+            }
+            setRankings(results);
+
+        } catch (e) {
+            setError(await handleFetchError(e));
+        }
+        console.log(rankings);
+    }
+
+    function renderList(rankings: Ranking[]) {
+        return <div>
+            {rankings.map(r => <p>
+                    {JSON.stringify(r)}
+                </p>)}
+        </div>
+    }
+    return <>
+    {rankings && renderList(rankings)}
+    </>
+}
--- client/src/pages/LeaderboardPage.tsx
+++ client/src/pages/LeaderboardPage.tsx
@@ -1,36 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { Ranking, LeaderboardApi, BoardType } from "../api";
-import handleFetchError from "../hooks/handleFetchError";
-
-const leaderboardApi = new LeaderboardApi();
+import {LeaderboardList} from "../components/leaderboards/LeaderboardList";
 
 export default function LeaderboardPage() {
-    const [rankings, setRankings] = useState<Ranking[]>();
-    const [error, setError] = useState<string>();
-
-    useEffect(() => {
-        getRankings();
-    }, []);
-
-    async function getRankings() {
-        try {
-            let results = [];
-            for (let type in BoardType) {
-                let ranking = await leaderboardApi.getLeaderboard({type: BoardType.Savings});
-                results.push(ranking);
-            }
-            setRankings(results);
-
-        } catch (e) {
-            setError(await handleFetchError(e));
-            console.log(e);
-        }
-        console.log(rankings);
-    }
-
     return <>
-        <div>
-            <p>{JSON.stringify(rankings)}</p>
+        <div className="box">
+            {LeaderboardList()}
         </div>
     </>
 }

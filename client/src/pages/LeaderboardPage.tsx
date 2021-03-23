42ae76b45b61b5d675a5cc3cdd42719664053491
Andy Phan <andytphan@outlook.com>
2021-03-22 21:11:36 -0400

    get leaderboards

--- client/src/pages/LeaderboardPage.tsx
+++ client/src/pages/LeaderboardPage.tsx
@@ -1,17 +1,36 @@
-import React, {useContext} from "react";
-import {UserContext} from "../contexts/UserContext";
-import {Redirect} from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Ranking, LeaderboardApi, BoardType } from "../api";
+import handleFetchError from "../hooks/handleFetchError";
+
+const leaderboardApi = new LeaderboardApi();
 
 export default function LeaderboardPage() {
-    const {isLoggedIn} = useContext(UserContext);
+    const [rankings, setRankings] = useState<Ranking[]>();
+    const [error, setError] = useState<string>();
+
+    useEffect(() => {
+        getRankings();
+    }, []);
+
+    async function getRankings() {
+        try {
+            let results = [];
+            for (let type in BoardType) {
+                let ranking = await leaderboardApi.getLeaderboard({type: BoardType.Savings});
+                results.push(ranking);
+            }
+            setRankings(results);
+
+        } catch (e) {
+            setError(await handleFetchError(e));
+            console.log(e);
+        }
+        console.log(rankings);
+    }
 
     return <>
-        {!isLoggedIn &&
-        <Redirect to="/"/>
-        }
-        <h1>Leaderboard</h1>
-        <div className="box">
-            <p>Coming Soon</p>
+        <div>
+            <p>{JSON.stringify(rankings)}</p>
         </div>
-    </>;
+    </>
 }

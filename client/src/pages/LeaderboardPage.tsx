2666357b793de6144d072e8cc271ededb9fb23c5
Andy Phan <andytphan@outlook.com>
2021-03-22 23:55:35 -0400

    format

--- client/src/pages/LeaderboardPage.tsx
+++ client/src/pages/LeaderboardPage.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
-import {LeaderboardList} from "../components/leaderboards/LeaderboardList";
+import { LeaderboardList } from "../components/leaderboards/LeaderboardList";
 
 export default function LeaderboardPage() {
     return <>
+        <h1>Your Leaderboards</h1>
         <div className="box">
-            {LeaderboardList()}
+            <LeaderboardList />
         </div>
     </>
 }

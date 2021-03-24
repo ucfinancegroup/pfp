20111dca056ad0b3152787a2c5639e9325d0eef4
c650 <c650@users.noreply.github.com>
2021-03-24 18:06:55 -0400

    add more peers (#278)

--- server/src/bin/generate_demo_users.rs
+++ server/src/bin/generate_demo_users.rs
@@ -1,6 +1,7 @@
 use chrono::Utc;
 use pfp_server::common::Money;
 use pfp_server::models::user_model::{Location, Snapshot, User};
+use rust_decimal::Decimal;
 use rust_decimal_macros::dec;
 use serde_json::to_string_pretty;
 use std::fs;
@@ -11,7 +12,7 @@ fn main() {
   let first_snapshot_time = (Utc::now() - chrono::Duration::days(3)).timestamp();
   let second_snapshot_time = Utc::now().timestamp();
 
-  let demo_users: Vec<User> = vec![
+  let mut demo_users: Vec<User> = vec![
     User {
       id: None,
       email: "hamilton@us.gov".to_string(),
@@ -39,9 +40,9 @@ fn main() {
         },
         Snapshot {
           net_worth: Money::new(dec!(1_000_000)),
-          running_income: Money::new(dec!(1_000)),
+          running_income: Money::new(dec!(6_000)),
           running_savings: Money::new(dec!(0)),
-          running_spending: Money::new(dec!(1_000)),
+          running_spending: Money::new(dec!(6_000)),
           snapshot_time: second_snapshot_time,
         },
       ],
@@ -78,7 +79,7 @@ fn main() {
         },
         Snapshot {
           net_worth: Money::new(dec!(1_001_000)),
-          running_income: Money::new(dec!(2_000)),
+          running_income: Money::new(dec!(25_000)),
           running_savings: Money::new(dec!(1_000)),
           running_spending: Money::new(dec!(1_000)),
           snapshot_time: second_snapshot_time,
@@ -119,7 +120,7 @@ fn main() {
           net_worth: Money::new(dec!(1_040_000)),
           running_income: Money::new(dec!(40_000)),
           running_savings: Money::new(dec!(1_000)),
-          running_spending: Money::new(dec!(1_000)),
+          running_spending: Money::new(dec!(40_000)),
           snapshot_time: second_snapshot_time,
         },
       ],
@@ -130,6 +131,48 @@ fn main() {
       rankings: vec![],
     },
   ];
+
+  for i in 0..20 {
+    demo_users.push(User {
+      id: None,
+      email: format!("demo{}@us.gov", i),
+      password: default_password.clone(),
+      first_name: "Alexander".to_string(),
+      last_name: "Hamilton".to_string(),
+      income: dec!(30_000.0),
+      net_worth: dec!(100_000.00),
+      location: Location {
+        // new york city
+        has_location: false,
+        lat: 0.0,
+        lon: 0.0,
+      },
+      birthday: format!("{}-03-03", 2000 - i),
+      accounts: vec![],
+      account_records: vec![],
+      snapshots: vec![
+        Snapshot {
+          net_worth: Money::new(dec!(100_000)),
+          running_income: Money::new(dec!(0)),
+          running_savings: Money::new(dec!(0)),
+          running_spending: Money::new(dec!(0)),
+          snapshot_time: first_snapshot_time,
+        },
+        Snapshot {
+          net_worth: Money::new(dec!(100_000)),
+          running_income: Money::new(dec!(1_000) * Decimal::from(i)),
+          running_savings: Money::new(dec!(0)),
+          running_spending: Money::new(dec!(1_000) * Decimal::from(i)),
+          snapshot_time: second_snapshot_time,
+        },
+      ],
+      recurrings: vec![],
+      goals: vec![],
+      insights: vec![],
+      plans: vec![],
+      rankings: vec![],
+    })
+  }
 
   let s = to_string_pretty(&demo_users).unwrap();
   let _ = fs::write("file", s).unwrap();

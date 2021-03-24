e3ceaf317dc8a03db4514897493c37ed1a01c73e
Andy Phan <andytphan@outlook.com>
2021-03-23 21:47:09 -0400

    redirect

--- client/src/pages/LeaderboardPage.tsx
+++ client/src/pages/LeaderboardPage.tsx
@@ -1,8 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, {useContext, useEffect, useState} from "react";
 import { LeaderboardList } from "../components/leaderboards/LeaderboardList";
+import {UserContext} from "../contexts/UserContext";
+import {Redirect, useHistory, useLocation} from "react-router-dom";
 
 export default function LeaderboardPage() {
+    const {isLoggedIn} = useContext(UserContext);
     return <>
+        {!isLoggedIn &&
+            <Redirect to="/"/>
+        }
         <h1>Your Leaderboards</h1>
         <div className="box">
             <LeaderboardList />

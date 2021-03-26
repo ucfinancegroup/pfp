3f57ebe136559e3ea9763c0cf72e4081e4566981
Andy Phan <andytphan@outlook.com>
2021-03-26 13:29:04 -0400

    info button

--- client/src/components/leaderboards/LeaderboardList.tsx
+++ client/src/components/leaderboards/LeaderboardList.tsx
@@ -42,13 +42,16 @@ export function LeaderboardList() {
             <ul className="nav nav-pills mt-4 mb-4">
                 {rankings.map(rank => {
                     return <li className={styles.board}>
-                        <div className={styles.center}>
-                            <h3>{rank.leaderboard_type}</h3>
-                            <p>You are better than <strong>
-                                <span className=
-                                    {(rank.percentile < 50) ? styles.red : styles.green}>
-                                    {(100 - rank.percentile).toFixed(1)}%
+                        <div>
+                            <div className={styles.center}>
+                                <h3>{rank.leaderboard_type}</h3>
+                                <p>You are better than <strong>
+                                    <span className=
+                                        {(rank.percentile < 50) ? styles.red : styles.green}>
+                                        {(100 - rank.percentile).toFixed(1)}%
                             </span></strong> of similar users by {rank.leaderboard_type}.</p>
+                            </div>
+                            <div className={styles.info}><i className="fa fa-info" aria-hidden="true"/></div>
                         </div>
                         <svg className={styles.meter}>
                             <defs>
@@ -63,7 +66,7 @@ export function LeaderboardList() {
                             </circle>
                             <circle r="4em" cx="70%" cy="50%" stroke="url(#linear)"
                                 stroke-width="1em"
-                                stroke-dasharray={`${rank.percentile/100 * 8 * Math.PI}em, 2000`}
+                                stroke-dasharray={`${rank.percentile / 100 * 8 * Math.PI}em, 2000`}
                                 fill="none">
                             </circle>
 
